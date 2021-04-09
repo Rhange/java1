99bd98f444fee42ece79150580f8626f7852d1f8
Rhange <rhange0511@gmail.com>
2021-04-09 16:59:54 +0900

    Instance를 활용한 예제 코드
    - 클래스만으로는 깔끔한 코드를 작성할 수 없는 상황에서 `인스턴스`의 존재가 나타난다.

--- MyApp/AccountingClassApp.java
+++ MyApp/AccountingClassApp.java
@@ -1,61 +1,82 @@
 class Accounting {
-	
-	public static double valueOfSupply;
-	public static double vatRate;
-	public static double expenseRate;
-	
-	public static void print() {
+
+	public double valueOfSupply;
+	public double vatRate;
+	public double expenseRate;
+
+	public void print() {
 		System.out.println("Value of supply : " + valueOfSupply);
-		System.out.println("VAT : "+ getVAT());
-		System.out.println("Total : "+ getTotal());
-		System.out.println("Expanse : "+ getExpense());
-		System.out.println("Income : "+ getIncome());
-		System.out.println("Dividend 1 : "+ getDividend1());
-		System.out.println("Dividend 2 : "+ getDividend2());
-		System.out.println("Dividend 3 : "+ getDividend3());
+		System.out.println("VAT : " + getVAT());
+		System.out.println("Total : " + getTotal());
+		System.out.println("Expanse : " + getExpense());
+		System.out.println("Income : " + getIncome());
+		System.out.println("Dividend 1 : " + getDividend1());
+		System.out.println("Dividend 2 : " + getDividend2());
+		System.out.println("Dividend 3 : " + getDividend3());
+		System.out.println("===============================");
 	}
 
-	public static double getDividend3() {
+	public double getDividend3() {
 		return getIncome() * 0.2;
 	}
 
-	public static double getDividend2() {
+	public double getDividend2() {
 		return getIncome() * 0.3;
 	}
 
-	public static double getDividend1() {
+	public double getDividend1() {
 		return getIncome() * 0.5;
 	}
 
-	public static double getIncome() {
+	public double getIncome() {
 		return valueOfSupply - getExpense();
 	}
 
-	public static double getExpense() {
+	public double getExpense() {
 		return valueOfSupply * expenseRate;
 	}
 
-	public static double getTotal() {
+	public double getTotal() {
 		return valueOfSupply + getVAT();
 	}
 
-	public static double getVAT() {
+	public double getVAT() {
 		return valueOfSupply * vatRate;
 	}
-	
+
 }
 
 public class AccountingClassApp {
 
 	// main method
-	public static void main(String[] args) {	
-		Accounting.valueOfSupply = 10000.0;
-		Accounting.vatRate = 0.1;
-		Accounting.expenseRate = 0.3;
-		Accounting.print();
-		// 다른 역할을 하는 변수와 매소드가 (1억개가) 있다면? 
-		// anotherVariable = ...;
-		// anotherMethod = ...;
+	public static void main(String[] args) {
+//		Accounting.valueOfSupply = 10000.0;
+//		Accounting.vatRate = 0.1;
+//		Accounting.expenseRate = 0.3;
+		// ...
+//		Accounting.print();
+		// ...
+		// 다른 제품이 들어온다면?
+//		Accounting.valueOfSupply = 20000.0;
+//		Accounting.vatRate = 0.05;
+//		Accounting.expenseRate = 0.2;
+//		Accounting.print();
+		// ...
+		// 다시 이전에 했던 데이터를 사용해야 한다면?
+
+		Accounting a1 = new Accounting(); // instance 생성
+		a1.valueOfSupply = 10000.0;
+		a1.vatRate = 0.1;
+		a1.expenseRate = 0.3;
+		a1.print();
+
+		Accounting a2 = new Accounting(); // instance 생성
+		a2.valueOfSupply = 20000.0;
+		a2.vatRate = 0.05;
+		a2.expenseRate = 0.2;
+		a2.print();
+
+		a1.print();
 	}
 
 }
